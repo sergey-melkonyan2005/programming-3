dade61473038ad9711021913be0952e3b0479814
user <user@lab657m.tumo.lab>
2019-06-02 17:40:59 +0400

    kiraki

---
+++ Programming 3/Lesson 2/GrassEater.js
@@ -0,0 +1,93 @@
+class GrassEater {
+    constructor(x, y, index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+        this.energy = 5;
+    }
+    //vorpes method
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    //qayluma
+    move() {
+
+        //yntruma vandak
+        var newCell = random(this.chooseCell(0));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = this.index;
+
+            this.y = newY;
+            this.x = newX;
+            this.energy--;
+
+        }
+
+    }
+    eat() {
+
+
+        var newCell = random(this.chooseCell(1));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = this.index;
+
+            for (var i in grassArr) {
+                if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+
+
+            this.y = newY;
+            this.x = newX;
+            this.energy += 2;
+
+        }
+    }
+    mul() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (this.energy >= 8 && newCell) {
+            var newGrassEater = new GrassEater(newCell[0], newCell[1], this.index);
+            grassEaterArr.push(newGrassEater);
+            matrix[newCell[1]][newCell[0]] = 2;
+            this.energy = 5;
+        }
+    }
+}
---
+++ Programming 3/Lesson 2/YellowEater.js
@@ -0,0 +1,101 @@
+class YellowEater extends Jarangutyun {
+    constructor(x, y, index){
+        super(x, y, index);
+    }
+
+    //vorpes method
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+    //qayluma
+    move() {
+
+        //yntruma vandak
+        var newCell = random(this.chooseCell(0));
+        this.energy--;
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = 3;
+
+
+            this.y = newY;
+            this.x = newX;
+
+        }
+    }
+    eat() {
+
+
+        var newCell = random(this.chooseCell(2));
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = 3;
+
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }
+            }
+
+
+            this.y = newY;
+            this.x = newX;
+            this.energy += 2;
+
+        }
+    }
+    mul() {
+
+        var newCell = random(this.chooseCell(0));
+
+        if (this.energy >= 8 && newCell) {
+            var newYellowEater = new YellowEater(newCell[0], newCell[1], this.index);
+            yellowEaterArr.push(newYellowEater);
+            matrix[newCell[1]][newCell[0]] = 3;
+            this.energy = 5;
+        }
+    }
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.x][this.y] = 0;
+            for (var i in yellowEaterArr) {
+                if (this.x == yellowEaterArr[i].x && this.y == yellowEaterArr[i].y) {
+                    yellowEaterArr.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+}
